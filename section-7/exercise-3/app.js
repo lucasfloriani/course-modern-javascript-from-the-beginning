90e896f46eb21b87a05e107e874472739394a4d8
Lucas Floriani <lucas.floriane@gmail.com>
2018-03-27 23:00:17 -0300

    Best example with best API, Chuck Norris Joke Generator kkkk

---
+++ section-7/exercise-3/app.js
@@ -0,0 +1,32 @@
+document.querySelector('.get-jokes').addEventListener('click', getJokes);
+
+function getJokes(e) {
+  const number = document.querySelector('input[type="number"]').value;
+  const numberValue = number === '' ? 1 : number;
+
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', `http://api.icndb.com/jokes/random/${numberValue}`, true);
+
+  xhr.onload = function() {
+    if(this.status === 200) {
+      const response = JSON.parse(this.responseText);
+
+      let output = '';
+
+      if(response.type === 'success') {
+        response.value.forEach(function(joke) {
+          output += `<li>${joke.joke}</li>`;
+        });
+      } else {
+        output += '<li>Something went wrong</li>';
+      }
+
+      document.querySelector('.jokes').innerHTML = output;
+    }
+  }
+
+  xhr.send();
+
+  e.preventDefault();
+}
