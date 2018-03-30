03ada0f8e92481337e18510070fc0ab8d66e50fa
Lucas Floriani <lucas.floriane@gmail.com>
2018-03-30 20:34:57 -0300

    EasyHTTP v2: Refactor with es6 with fetch, class and arrow functions

---
+++ section-7/exercise-10/easyhttp2.js
@@ -0,0 +1,68 @@
+/**
+ * EasyHTTP Library
+ * Library for making HTTP requests
+ *
+ * @version 2.0.0
+ * @author Lucas Floriani
+ * @license MIT
+ *
+ */
+
+class EasyHTTP {
+  // Make an HTTP GET Request
+  get(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url)
+        .then(res => res.json())
+        .then(data => resolve(data))
+        .catch(err => reject(err))
+    });
+  }
+
+  // Make an HTTP POST Request
+  post(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+        .then(res => res.json())
+        .then(data => resolve(data))
+        .catch(err => reject(err))
+    });
+  }
+
+  // Make an HTTP Put Request
+  put(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+        .then(res => res.json())
+        .then(data => resolve(data))
+        .catch(err => reject(err))
+    });
+  }
+
+  // Make an HTTP DELETE Request
+  delete(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: 'DELETE',
+        headers: {
+          'Content-type': 'application/json'
+        }
+      })
+        .then(res => res.json())
+        .then(() => resolve('Resource Deleted...'))
+        .catch(err => reject(err))
+    });
+  }
+ }
