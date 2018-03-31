6972789f4c7ba240a38b633b394619a28091bfdd
Lucas Floriani <lucas.floriane@gmail.com>
2018-03-31 20:55:33 -0300

    WeatherJS - Part 4: Save Location to Local Storage

--- section-8/weatherjs/app.js
+++ section-8/weatherjs/app.js
@@ -1,5 +1,10 @@
+// Init storage
+const storage = new Storage();
+// Get stored location data
+const weatherLocation = storage.getLocationData();
+
 // Init weather object
-const weather = new Weather('Boston', 'MA');
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
 
 // Init UI object
 const ui = new UI();
@@ -7,7 +12,23 @@ const ui = new UI();
 // Get weather on DOM load
 document.addEventListener('DOMContentLoaded', getWeather);
 
-// weather.changeLocation('Miami', 'FL');
+// Change location event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+  const city = document.getElementById('city').value;
+  const state = document.getElementById('state').value;
+
+  // Change location
+  weather.changeLocation(city, state);
+
+  // Set location in LS
+  storage.setLocationData(city, state);
+
+  // Get and display weather
+  getWeather();
+
+  // Clode modal
+  $('#locModal').modal('hide');
+})
 
 function getWeather() {
   weather.getWeather()
