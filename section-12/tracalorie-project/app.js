ba8e0694e639644e2605fdc2197704e5226b078f
Lucas Floriani <lucas.floriane@gmail.com>
2018-04-24 21:53:30 -0300

    Tracalorie Project: Add item to data structure

--- section-12/tracalorie-project/app.js
+++ section-12/tracalorie-project/app.js
@@ -25,6 +25,26 @@ const ItemCtrl = (function() {
     getItems: function() {
       return data.items;
     },
+    addItem: function(name, calories) {
+      let ID;
+      // Create ID
+      if(data.items.length > 0) {
+        ID = data.items[data.items.length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+
+      // Calories to number
+      calories = parseInt(calories);
+
+      // Create new item
+      newItem = new Item(ID, name, calories);
+
+      // Add to items array
+      data.items.append(newItem);
+
+      return newItem;
+    },
     logData: function() {
       return data;
     }
@@ -34,7 +54,10 @@ const ItemCtrl = (function() {
 // UI Controller
 const UICtrl = (function() {
   const UISelectors = {
-    itemList: '#item-list'
+    itemList: '#item-list',
+    addBtn: '.add-btn',
+    itemNameInput: '#item-name',
+    itemCaloriesInput: '#item-calories'
   }
 
   // Public Methods
@@ -51,12 +74,45 @@ const UICtrl = (function() {
 
       // Insert list items
       document.querySelector(UISelectors.itemList).innerHTML = html;
+    },
+    getItemInput: function() {
+      return {
+        name: document.querySelector(UISelectors.itemNameInput).value,
+        calories: document.querySelector(UISelectors.itemCaloriesInput).value
+      }
+    },
+    getSelectors: function() {
+      return UISelectors;
     }
   }
 })();
 
 // App Controller
 const App = (function(ItemCtrl, UICtrl) {
+  // Load event listeners
+  const loadEventListeners = function() {
+    // Get UI Selectors
+    const UISelectors = UICtrl.getSelectors()
+
+    // Add item event
+    document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
+  }
+
+  // Add item submit
+  const itemAddSubmit = function(e) {
+    e.preventDefault();
+    // Get form input from UI Controller
+    const input = UICtrl.getItemInput();
+
+    // Check for name and calories input
+    if(input.name === '' || input.calories === '') {
+      return
+    }
+    // Add item
+    const newItem = ItemCtrl.addItem(input.name, input.calories);
+
+    console.log(input);
+  }
 
   // Public Methods
   return {
@@ -66,6 +122,9 @@ const App = (function(ItemCtrl, UICtrl) {
 
       // Populate list with items
       UICtrl.populateItemList(items);
+
+      // Load event listeners
+      loadEventListeners();
     }
   }
 
