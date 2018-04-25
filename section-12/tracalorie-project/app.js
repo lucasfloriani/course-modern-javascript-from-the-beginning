d9fe0f2165a04d74090e01f738f58078adacf823
Lucas Floriani <lucas.floriane@gmail.com>
2018-04-24 21:31:57 -0300

    Tracalorie Project: Get and populate items

--- section-12/tracalorie-project/app.js
+++ section-12/tracalorie-project/app.js
@@ -22,6 +22,9 @@ const ItemCtrl = (function() {
 
   // Public Methods
   return {
+    getItems: function() {
+      return data.items;
+    },
     logData: function() {
       return data;
     }
@@ -30,10 +33,25 @@ const ItemCtrl = (function() {
 
 // UI Controller
 const UICtrl = (function() {
+  const UISelectors = {
+    itemList: '#item-list'
+  }
 
   // Public Methods
   return {
+    populateItemList: function(items) {
+      let html = '';
 
+      items.forEach(function(item) {
+        html += `<li class="collection-item" id="item-${item.id}">
+          <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+          <a href="#" class="secondary-content"><i class="edit-item fa fa-pencil"></i></a>
+        </li>`;
+      });
+
+      // Insert list items
+      document.querySelector(UISelectors.itemList).innerHTML = html;
+    }
   }
 })();
 
@@ -43,7 +61,11 @@ const App = (function(ItemCtrl, UICtrl) {
   // Public Methods
   return {
     init: function() {
-      console.log('Initializing App...');
+      // Fetch items from data structure
+      const items = ItemCtrl.getItems();
+
+      // Populate list with items
+      UICtrl.populateItemList(items);
     }
   }
 
