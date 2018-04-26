7917b36671b9f81d8f2fe06c3fd1342e3fd559c8
Lucas Floriani <lucas.floriane@gmail.com>
2018-04-25 21:30:29 -0300

    Tracalorie Project: Add and get from local storage

--- section-12/tracalorie-project/app.js
+++ section-12/tracalorie-project/app.js
@@ -1,4 +1,29 @@
 // Storage Controller
+const StorageCtrl = (function() {
+  // Public Methods
+  return {
+    storeItem: function(item) {
+      let items = [];
+      // Check if any items in ls
+      if(localStorage.getItem('items') !== null) {
+        items = JSON.parse(localStorage.getItem('items'));
+      }
+      // Push new item
+      items.push(item);
+      // Set ls
+      localStorage.setItem('items', JSON.stringify(items));
+    },
+    getItemsFromStorage: function() {
+      let items;
+      if(localStorage.getItem('items') === null) {
+        items = [];
+      } else {
+        items = JSON.parse(localStorage.getItem('items'));
+      }
+      return items;
+    }
+  }
+})();
 
 // Item Controller
 const ItemCtrl = (function() {
@@ -11,11 +36,12 @@ const ItemCtrl = (function() {
 
   // Data Structure / State
   const data = {
-    items: [
-      // {id: 0, name: "Steak Dinner", calories: 1200},
-      // {id: 1, name: "Cookie", calories: 400},
-      // {id: 2, name: "Eggs", calories: 300},
-    ],
+    // items: [
+    //   // {id: 0, name: "Steak Dinner", calories: 1200},
+    //   // {id: 1, name: "Cookie", calories: 400},
+    //   // {id: 2, name: "Eggs", calories: 300},
+    // ],
+    items: StorageCtrl.getItemsFromStorage(),
     currentItem: null,
     totalCalories: 0
   }
@@ -233,7 +259,7 @@ const UICtrl = (function() {
 })();
 
 // App Controller
-const App = (function(ItemCtrl, UICtrl) {
+const App = (function(ItemCtrl, UICtrl, StorageCtrl) {
   // Load event listeners
   const loadEventListeners = function() {
     // Get UI Selectors
@@ -286,6 +312,9 @@ const App = (function(ItemCtrl, UICtrl) {
 
     // Add total calories to UI
     UICtrl.showTotalCalories(totalCalories);
+
+    // Store in localStorage
+    StorageCtrl.storeItem(newItem);
 
     // Clear fields
     UICtrl.clearInput();
@@ -406,7 +435,7 @@ const App = (function(ItemCtrl, UICtrl) {
     }
   }
 
-})(ItemCtrl, UICtrl);
+})(ItemCtrl, UICtrl, StorageCtrl);
 
 // Initialize App
 App.init();
