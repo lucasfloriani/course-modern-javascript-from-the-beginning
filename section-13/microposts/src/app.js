a1b73518133440b920cf67f6434e0c1906bec71c
Lucas Floriani <lucas.floriane@gmail.com>
2018-04-27 21:24:26 -0300

    Microposts Project: Add posts and show alert

--- section-13/microposts/src/app.js
+++ section-13/microposts/src/app.js
@@ -4,8 +4,31 @@ import { ui } from './ui.js';
 // Get posts on DOM load
 document.addEventListener('DOMContentLoaded', getPosts);
 
+// Listen for add post
+document.querySelector('.post-submit').addEventListener('click', submitPost);
+
+// Get Posts
 function getPosts() {
   http.get('http://localhost:3000/posts')
   .then(data => ui.showPosts(data))
+  .catch(err => console.log(err));
+}
+
+// Submit Post
+function submitPost() {
+  const title = document.getElementById('title').value;
+  const body = document.getElementById('body').value;
+
+  const data = {
+    title,
+    body
+  }
+
+  http.post('http://localhost:3000/posts', data)
+  .then(data => {
+    ui.showAlert('Post added', 'alert alert-success');
+    ui.clearFields();
+    getPosts();
+  })
   .catch(err => console.log(err));
 }
--- section-13/microposts/src/ui.js
+++ section-13/microposts/src/ui.js
@@ -30,6 +30,41 @@ class UI {
 
     this.post.innerHTML = output;
   }
+
+  showAlert(message, className) {
+    this.clearAlert();
+
+    // Create div
+    const div = document.createElement('div');
+    // Add classes
+    div.className = className;
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    // Get parent
+    const container = document.querySelector('.postsContainer');
+    // Get posts
+    const posts = document.querySelector('#posts');
+    // Append message
+    container.insertBefore(div, posts);
+
+    // Timeout
+    setTimeout(() => {
+      this.clearAlert();
+    }, 3000);
+  }
+
+  clearAlert() {
+    const currentAlert = document.querySelector('.alert');
+
+    if(currentAlert) {
+      currentAlert.remove();
+    }
+  }
+
+  clearFields() {
+    this.titleInput.value = '';
+    this.bodyInput.value = '';
+  }
 }
 
 export const ui = new UI();
